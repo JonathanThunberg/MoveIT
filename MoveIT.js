fb0746224ea29f573aa1539cd08d8e51a4f5c03c
Emil Johansson <emij@student.chalmers.se>
2015-06-30 11:38:09 +0200

    added a collection

--- MoveIT.js
+++ MoveIT.js
@@ -1,4 +1,10 @@
 if (Meteor.isClient) {
+  var TenMinutes = new Mongo.Collection('tenMinutes');
+  TenMinutes.insert({ id:1, text: "Hello World" });
+  console.log(TenMinutes);
+
+
+
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -9,10 +15,20 @@ if (Meteor.isClient) {
   });
 
   Template.hello.events({
-    'click button': function () {
+    'click #plus': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+    },
+    'click #minus': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') - 1);
+    },
+    'click #db': function () {
+      // increment the counter when button is clicked
+      var text = TenMinutes.findOne({id: 1}).fetch();
+      console.log(text);
     }
+
   });
 }
 
