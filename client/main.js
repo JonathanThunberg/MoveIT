b520f9ede0f818b2170b26ee8688fa472f7be7df
Erik Axelsson <erikaxelsson1@gmail.com>
2015-06-30 16:49:58 +0200

    add acceleromenter

--- client/main.js
+++ client/main.js
@@ -1,29 +1,44 @@
 var walkingService = new walkService();
 
-  // counter starts at 0
-  Session.setDefault('level', walkingService.getCurrent());
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log("onDeviceReady");
+    console.log(JSON.stringify(navigator.accelerometer));
+    var watchID = navigator.accelerometer.watchAcceleration(
+      function(acceleration){
+        console.log('Acceleration X: ' + acceleration.x +" : " +
+           'Acceleration Y: ' + acceleration.y + " : " +
+           'Acceleration Z: ' + acceleration.z + " : " +
+           'Timestamp: '      + acceleration.timestamp );
+      },
+      function(){
 
-  Template.hello.helpers({
-    level: function () {
-      return Session.get('level');
-    }
-  });
-  Template.hello.events({
-    'click #plus': function () {
-      // increment the level when button is clicked
-      walkingService.takeStep();
-      Session.set('level', walkingService.getCurrent());
-    },
-    'click #minus': function () {
-      // increment the level when button is clicked
-      walkingService.lowerLevel();
-      Session.set('level', walkingService.getCurrent());
-    },
-    'click #db': function () {
-      console.log(walkingService.countCollection());
-    },
-    'click #cleanCollection': function () {
-      walkingService.cleanCollection();
-    }
+      },{ frequency: 3000 });
+}
+// counter starts at 0
+Session.setDefault('level', walkingService.getCurrent());
 
-  });
+Template.hello.helpers({
+  level: function () {
+    return Session.get('level');
+  }
+});
+Template.hello.events({
+  'click #plus': function () {
+    // increment the level when button is clicked
+    walkingService.takeStep();
+    Session.set('level', walkingService.getCurrent());
+  },
+  'click #minus': function () {
+    // increment the level when button is clicked
+    walkingService.lowerLevel();
+    Session.set('level', walkingService.getCurrent());
+  },
+  'click #db': function () {
+    console.log(walkingService.countCollection());
+  },
+  'click #cleanCollection': function () {
+    walkingService.cleanCollection();
+  }
+
+});
