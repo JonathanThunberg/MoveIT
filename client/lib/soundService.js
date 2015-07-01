2817ee5c4ab79bd9bb44d5833a6a009cbbe591f2
Emil Johansson <emij@student.chalmers.se>
2015-07-01 16:27:21 +0200

    changed sound

--- client/lib/soundService.js
+++ client/lib/soundService.js
@@ -1,29 +1,15 @@
 soundService = function() {
-
+  var media;
   document.addEventListener("deviceready", onDeviceReady, false);
   function onDeviceReady() {
-    var media = new Media(src('/sounds/test.wav'), mediaSuccess, [mediaError], [mediaStatus]);
-  }
-  function getMediaUrl(sound) {
-    if (device.platform.toLowerCase() === "android") {
-      return cordova.file.applicationDirectory.replace('file://', '') + 'www/application/' + sound.substr(1);
-    } else {
-      return cordova.file.applicationDirectory.replace('file://', '') + sound.substr(1);
-    }
-  }
-  function playSound(sound) {
-    return new Media(
-      getMediaUrl(sound),
-      function (success) {
-        // success
-      },
-      function (err) {
-        // error
-      }
-    );
-  }
+    media = new Media('www/application/sounds/test.wav', mediaSuccess, [mediaError], [mediaStatus]);
+  };
+  function mediaSuccess(){
+
+  };
   var test = playSound('/sounds/test.wav');
-  this.playSound() {
-    test.play();
+  this.playSound = function() {
+    media.play();
   }
+
 }
