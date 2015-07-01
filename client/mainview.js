30fbd5c688e537b4f051dfbc48cb79494d836a74
Heff <heff@heff.se>
2015-07-01 12:57:29 +0200

    Added prototyped settings buttons for alarm settings.

--- client/mainview.js
+++ client/mainview.js
@@ -16,6 +16,11 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  // all settings enabled by default for now
+  Session.setDefault('alarmUseSound', true);
+  Session.setDefault('alarmUseVibration', true);
+  Session.setDefault('alarmUseLight', true);
+
   Meteor.startup(function () {
     updateActivity();
   });
@@ -31,6 +36,49 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
       updateActivity();
+    }
+  });
+
+  Template.body.helpers({
+    // Data context for alarm settings buttons:
+    alarmSettings: [
+      { type: 'Sound',
+        icon_enabled: 'volume_up',
+        icon_disabled: 'volume_off' },
+
+      { type: 'Vibration',
+        icon_enabled: 'alarm_on',
+        icon_disabled: 'alarm_off' },
+
+      { type: 'Light',
+        icon_enabled: 'phonelink_ring',
+        icon_disabled: 'phonelink_erase' }
+    ]
+  });
+
+  //---------------------------
+  // Quick settings buttons
+  //---------------------------
+  Template.alarmSettingBtn.events({
+    'click': function () {
+      Session.set('alarmUse' + this.type, !Session.get('alarmUse' + this.type));
+    }
+  });
+
+  Template.alarmSettingBtn.helpers({
+    'state': function() {
+      if (Session.get('alarmUse' + this.type)) {
+        return '';
+      } else {
+        return 'disabled';
+      }
+    },
+    'icon': function() {
+      if (Session.get('alarmUse' + this.type)) {
+        return this.icon_enabled;
+      } else {
+        return this.icon_disabled;
+      }
     }
   });
 }
