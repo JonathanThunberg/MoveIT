199ed855bce2d3c74bfea13be58fa7f7695ba6d1
Emil Johansson <emij@student.chalmers.se>
2015-07-01 10:54:30 +0200

    added acc output

--- client/lib/walkService.js
+++ client/lib/walkService.js
@@ -11,6 +11,13 @@
     var timeInterval = 3000;
     var updateInterval = 10000;
     var accUpdateFreq = 3000;
+    var oldAcc;
+    var acc = {
+        x: 0,
+        y: 0,
+        z: 0,
+        timestamp: 0
+    };
 
     function updateCollection() {
         tenMinutes.insert({timestamp: new Date().getTime(), level: currentActivityLevel})
@@ -29,7 +36,16 @@
         }, timeInterval);
 
     function AccSuccess(acceleration) {
+        if(oldAcc !== null){
+            acc.x = (oldAcc.x - acceleration.x);
+            acc.y = (oldAcc.y - acceleration.y);
+            acc.z = (oldAcc.z - acceleration.z);
+        }
+        console.log('diff x: ' + acc.x);
+        console.log('diff y: ' + acc.y);
+        console.log('diff z: ' + acc.z);
 
+        oldAcc = acceleration;
     };
     function AccError() {
     };
