eee071ff3a3518ff5c179ed3fb94505f29d624e6
Krabban <anderssongabriel665@gmail.com>
2015-07-02 14:52:44 +0200

    Time selection works. Good stuff.

--- client/views/settings/settingsview.js
+++ client/views/settings/settingsview.js
@@ -3,13 +3,13 @@ if (Meteor.isClient) {
   Template.weekdaysettings.helpers({
     
     day: [
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-      'Sunday'
+      {name: 'Monday'},
+      {name: 'Tuesday'},
+      {name: 'Wednesday'},
+      {name: 'Thursday'},
+      {name: 'Friday'},
+      {name: 'Saturday'},
+      {name: 'Sunday'}
     ]
   });
 
@@ -29,4 +29,44 @@ if (Meteor.isClient) {
 	      Session.set('active' + this.day, !Session.get('active' + this.day));
 	    }
 	});
+
+    Template.timeselectsettings.helpers({ 
+    	event : [ 
+    		{name: 'Starts'},
+    		{name: 'Ends'}
+    		]
+    })
+    Template.timeselectitem.helpers({ 
+    	'time' : function() { 
+    		return Session.get('time'+this.name);
+    	} ,
+    	'formattedtime' : function() { 
+    		return minutesTohhmm(Session.get('time'+this.name));
+    	}
+
+    });
+
+    Template.timeslider.events({ 
+    	'change': function() { 
+    		var v = $("#time_slider_"+this.name).val();
+    			Session.set('time'+this.name, v);
+    	},
+    	'input': function(){
+    	    var v = $("#time_slider_"+this.name).val();
+    		if(v){ 
+    			Session.set('time'+this.name,v);
+    		}
+    	}
+    });
+
+
 }
+
+
+  	function minutesTohhmm(minutes){
+  		if(minutes===undefined) {return "0:00"};
+  		var h = Math.floor(minutes/60);
+  		var m = minutes%60;
+  		return (h<10?"0":"")+h+":"+
+  			   (m<10?"0":"")+m;
+  	}
