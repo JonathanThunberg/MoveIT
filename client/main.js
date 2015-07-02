beb014ce34a968c6cce1f15e414575f577e5800b
Emil Johansson <emij@student.chalmers.se>
2015-07-02 15:38:32 +0200

    testcode for service

--- client/main.js
+++ client/main.js
@@ -1,10 +1,71 @@
 var walkingService = new walkService();
 var soundingService = new soundService();
 var alarmingService = new alarmService();
-var testBackgrounding = new testBackground();
 
 // counter starts at 0
 Session.setDefault('level', walkingService.getCurrent());
+/*
+if(Meteor.isCordova){
+
+  var myService;
+
+  function getStatus() {
+     myService.getStatus(function(r){displayResult(r)}, function(e){displayError(e)});
+  }
+
+  function displayResult(data) {
+     alert("Is service running: " + data.ServiceRunning);
+  }
+
+  function displayError(data) {
+     console.log("We have an error");
+     console.log(json.EJSON.stringify(data, {indent: true}));
+  }
+  function updateHandler(data) {
+    if (data.LatestResult != null) {
+       try {
+          var resultMessage = document.getElementById("resultMessage");
+          resultMessage.innerHTML = data.LatestResult.Message;
+       } catch (err) {
+       }
+     }
+   }
+
+
+  function go() {
+    myService.getStatus(function(r){startService(r)}, function(e){displayError(e)});
+  };
+
+  function startService(data) {
+   if (data.ServiceRunning) {
+      enableTimer(data);
+   } else {
+      myService.startService(function(r){enableTimer(r)}, function(e){displayError(e)});
+   }
+  }
+
+  function enableTimer(data) {
+   if (data.TimerEnabled) {
+      registerForUpdates(data);
+   } else {
+      myService.enableTimer(5000, function(r){registerForUpdates(r)}, function(e){displayError(e)});
+   }
+  }
+
+  function registerForUpdates(data) {
+   if (!data.RegisteredForUpdates) {
+      myService.registerForUpdates(function(r){updateHandler(r)}, function(e){handleError(e)});
+   }
+  }
+
+  document.addEventListener('deviceready', function() {
+      var serviceName = 'com.red_folder.phonegap.plugin.backgroundservice.MyService';
+      var factory = cordova.require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService')
+      myService = factory.create(serviceName);
+
+      go();
+      }, true);
+ } */
 
 Template.hello.helpers({
   level: function () {
