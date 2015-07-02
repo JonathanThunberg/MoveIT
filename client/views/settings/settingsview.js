9d84b957f69fa826f33bfff9225c4f5a7e7cf8ca
Krabban <anderssongabriel665@gmail.com>
2015-07-02 17:08:09 +0200

    Work added to the irritation level settings. not as responsive as hoped.

--- client/views/settings/settingsview.js
+++ client/views/settings/settingsview.js
@@ -49,18 +49,97 @@ if (Meteor.isClient) {
     Template.timeslider.events({ 
     	'change': function() { 
     		var v = $("#time_slider_"+this.name).val();
-    			Session.set('time'+this.name, v);
+    		v = v - (v%5) + ((v%5>2.5)? 5 : 0);
+    		Session.set('time'+this.name, v);
     	},
     	'input': function(){
     	    var v = $("#time_slider_"+this.name).val();
+    		v = v - (v%5) + ((v%5>2.5)? 5 : 0);
     		if(v){ 
     			Session.set('time'+this.name,v);
     		}
     	}
     });
 
+	Template.alarmsettings.helpers({ 
+    	event : [ 
+    		{
+    			name: 'Sound',
+    			icon: 'volume_up'
+    		},
+    		{
+    			name: 'Vibration',
+    			icon: 'alarm_on'
+    		},
+    		{
+    			name: 'Light',
+    			icon: 'phonelink_ring'
+    		}
+    	]
+    });
+
+	Template.alarmsettingsrow.helpers({ 
+    	alarmselected : function() { 
+    		return Session.get('alarmUse' + this.name);
+    	},
+
+
+    	sliderdisabled : function() { 
+    		if(Session.get('alarmUse' + this.name)){ 
+    			return '';
+    		}
+    		return 'disabled';
+    	}
+    });
+
+	Template.alarmsettingscheck.helpers({ 
+    	alarmselected : function() { 
+    		return Session.get('alarmUse' + this.name);
+    	}
+    });
+
+	Template.alarmsettingsslider.helpers({ 
+    	sliderdisabled : function() { 
+    		if(Session.get('alarmUse' + this.name)){ 
+    			return '';
+    		}
+    		return 'disabled';
+    	},
+    	intensity : function() { 
+    		console.log("dood");
+    		console.log(Session.get('alarmIntensity+'+this.name));
+    		return Session.get('alarmIntensity+'+this.name);
+    	}
+    });
+
+    
+
+    Template.alarmsettingscheck.events({
+	    'change': function() {
+
+	      Session.set('alarmUse' + this.name, !Session.get('alarmUse' + this.name));
+	      console.log(this.name+":   "+Session.get('alarmUse' + this.name));
+	    }
+	});
+    
+    Template.timeslider.events({ 
+    	'change': function() { 
+    		console.log("kvack");
+    		console.log(this.name);
+    		var v = $("#alarm_slider_"+this.name).val();
+    		console.log(this.name+":   "+v);	
+    		Session.set('alarmIntensity'+this.name, v);
+    	},
+    	'input': function(){
+    	    var v = $("#alarm_slider_"+this.name).val();
+    		Session.set('alarmIntensity'+this.name, v);
+    	}
+    });
+
 
 }
+
+	
 
 
   	function minutesTohhmm(minutes){
